2873691a7c5d296517464e1a1c99ca13b155b914
sanagama <sanagama2@gmail.com>
2017-03-28 16:31:53 -0700

    redirect root to '/polls/'

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,18 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(url='/polls/')), # redirect requests to '/' to 'polls/'
 ]
+
+# This works too:
+#
+# from django.http import HttpResponseRedirect
+#
+# urlpatterns = [
+#   url(r'^$', lambda r: HttpResponseRedirect('polls/')), # redirect requests to '/' to 'polls/'   
+# ]    
+#
